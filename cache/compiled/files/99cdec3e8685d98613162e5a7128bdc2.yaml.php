aeffca2abf7117cc624831651ad2c486915fc827
Praktikant <praktikant@wbx.local>
2018-10-04 16:24:15 +0200

    empty cache/

--- cache/compiled/files/99cdec3e8685d98613162e5a7128bdc2.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/xampp/htdocs/user/config/streams.yaml',
-    'modified' => 1538399041,
-    'data' => [
-        
-    ]
-];
